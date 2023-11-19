52635051cdc4067de1a29eeeb1bfe7c8b79c913f
giu-giu <195412063@qq.com>
2023-11-19 19:23:52 +0800

    fix led

--- software/main_control/User/Source/main.c
+++ software/main_control/User/Source/main.c
@@ -1,83 +1,22 @@
-/**
-  ******************************************************************************
-  * @file    User/Source/main.c
-  * @author  wangxian
-  * @version V1.1.0
-  * @date    21-Sep-2016
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
-  ******************************************************************************
-  */
-
-/* STM32 Includes ------------------------------------------------------------*/
-#include "stm32f10x.h"
 #include "led.h"
+#include "delay.h"
+#include "sys.h"
+//ALIENTEK miniSTM32开发板实验1
+//跑马灯实验  
+//技术支持：www.openedv.com
+//广州市星翼电子科技有限公司
+ int main(void)
+ {	
+	delay_init();	    	 //延时函数初始化	  
+	LED_Init();		  	//初始化与LED连接的硬件接口
+	while(1)
+	{
+		GPIO_ResetBits(GPIOA,GPIO_Pin_8); //LED0输出低
+		GPIO_SetBits(GPIOD,GPIO_Pin_2);//LED1输出高
+		delay_ms(300);
+		GPIO_SetBits(GPIOA,GPIO_Pin_8);//LED0输出高
+		GPIO_ResetBits(GPIOD,GPIO_Pin_2);//LED1输出低
+		delay_ms(300);
+	}
+ }
 
-/* FreeRTOS Includes ---------------------------------------------------------*/
-// #include "FreeRTOS.h"
-// #include "task.h"
-// #include "queue.h"
-
-/** @addtogroup STM32F10x_StdPeriph_Template
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
-int main(void)
-{
-
-    /* Add your application code here
-     */
-
-    /* Infinite loop */
-    LED_Init();	
-    LED0=0;
-	LED1=1;
-    while (1)
-    {
-        
-    }
-}
-
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-    /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-    /* Infinite loop */
-    while (1)
-    {
-    }
-}
-#endif
-
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
