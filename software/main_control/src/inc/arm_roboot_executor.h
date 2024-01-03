5c43c2553c583400520053d3140f7a19884c5975
giu-giu <195412063@qq.com>
2024-01-03 22:49:21 +0800

    fix servo range.

--- software/main_control/src/inc/arm_roboot_executor.h
+++ software/main_control/src/inc/arm_roboot_executor.h
@@ -25,6 +25,24 @@
 #define SERVO_1_MIDDLE  90
 #define SERVO_1_LEFT    179
 
+#define SERVO_2_BACK    20
+#define SERVO_2_MIDDLE  90
+#define SERVO_2_FRONT   160
+
+#define SERVO_3_BACK    1
+#define SERVO_3_MIDDLE  90
+#define SERVO_3_FRONT   179
+
+#define SERVO_3_BACK    1
+#define SERVO_3_MIDDLE  90
+#define SERVO_3_FRONT   179
+
+#define SERVO_5_HOR     90
+#define SERVO_5_VER     179
+
+#define SERVO_6_OPEN    90
+#define SERVO_6_CLOSE   179
+
 extern const struct module_command_executor arm_roboot_executor;
 
 #endif
