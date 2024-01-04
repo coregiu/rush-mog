e1aab7548a1551f529e0fa49763f70017ad7d285
giu-giu <195412063@qq.com>
2024-01-05 00:08:10 +0800

    fix all servo range.

--- software/main_control/src/inc/arm_roboot_executor.h
+++ software/main_control/src/inc/arm_roboot_executor.h
@@ -21,27 +21,33 @@
 #define SERVO_6 11
 
 // ---------------servo define----------------
-#define SERVO_1_RIGHT   1
-#define SERVO_1_MIDDLE  90
-#define SERVO_1_LEFT    179
+#define SERVO_RANGE_1_RIGHT   1
+#define SERVO_RANGE_1_MIDDLE  90
+#define SERVO_RANGE_1_LEFT    179
+#define SERVO_RANGE_1_INIT    90
 
-#define SERVO_2_BACK    20
-#define SERVO_2_MIDDLE  90
-#define SERVO_2_FRONT   160
+#define SERVO_RANGE_2_BACK    20
+#define SERVO_RANGE_2_MIDDLE  90
+#define SERVO_RANGE_2_FRONT   160
+#define SERVO_RANGE_2_INIT    20
 
-#define SERVO_3_BACK    1
-#define SERVO_3_MIDDLE  90
-#define SERVO_3_FRONT   179
+#define SERVO_RANGE_3_BACK    1
+#define SERVO_RANGE_3_MIDDLE  90
+#define SERVO_RANGE_3_FRONT   179
+#define SERVO_RANGE_3_INIT    110
 
-#define SERVO_4_BACK    1
-#define SERVO_4_MIDDLE  90
-#define SERVO_5_FRONT   179
+#define SERVO_RANGE_4_BACK    1
+#define SERVO_RANGE_4_MIDDLE  90
+#define SERVO_RANGE_4_FRONT   179
+#define SERVO_RANGE_4_INIT    70
 
-#define SERVO_5_HOR     90
-#define SERVO_5_VER     179
+#define SERVO_RANGE_5_HOR     90
+#define SERVO_RANGE_5_VER     179
+#define SERVO_RANGE_5_INIT    90
 
-#define SERVO_6_OPEN    179
-#define SERVO_6_CLOSE   120
+#define SERVO_RANGE_6_OPEN    120
+#define SERVO_RANGE_6_CLOSE   179
+#define SERVO_RANGE_6_INIT    179
 
 extern const struct module_command_executor arm_roboot_executor;
 
--- software/main_control/src/src/arm_roboot_executor.c
+++ software/main_control/src/src/arm_roboot_executor.c
@@ -13,12 +13,12 @@
 
 void init_roboot_state()
 {
-    pca_servo_init(SERVO_1, 60, 90);
-    pca_servo_init(SERVO_2, 60, 90);
-    pca_servo_init(SERVO_3, 60, 90);
-    pca_servo_init(SERVO_4, 60, 90);
-    pca_servo_init(SERVO_5, 60, 90);
-    pca_servo_init(SERVO_6, 60, 160);
+    pca_servo_init(SERVO_1, 60, SERVO_RANGE_1_INIT);
+    pca_servo_init(SERVO_2, 60, SERVO_RANGE_2_INIT);
+    pca_servo_init(SERVO_3, 60, SERVO_RANGE_3_INIT);
+    pca_servo_init(SERVO_4, 60, SERVO_RANGE_4_INIT);
+    pca_servo_init(SERVO_5, 60, SERVO_RANGE_5_INIT);
+    pca_servo_init(SERVO_6, 60, SERVO_RANGE_6_INIT);
 }
 
 void update_roboot_state(struct command_context *command_context)
