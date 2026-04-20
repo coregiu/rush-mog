3d3229893fb454dec4af57084242d132994d8be2
coregiu <195412063@qq.com>
2026-04-20 23:10:29 +0800

    add listener of keys.

--- software/remote_center/src/main.cpp
+++ software/remote_center/src/main.cpp
@@ -4,7 +4,7 @@
 
 // 【自己的 WiFi 热点设置】手机直接连这个
 #define AP_SSID "RUSH-MOG"          // 热点名称
-#define AP_PSW "coregiu_927"            // 热点密码（至少8位）
+#define AP_PSW "coregiu_927"        // 热点密码（至少8位）
 IPAddress local_IP(192, 168, 4, 1); // 本地IP
 IPAddress gateway(192, 168, 4, 1);  // 网关
 IPAddress subnet(255, 255, 255, 0); // 子网掩码
@@ -42,6 +42,23 @@ void handleWebRequest()
   }
 }
 
+void handleCmdButton() {
+  // 定义按钮
+  String key = server.arg("key");
+  // 定义事件类型：按下还是松开
+  String type = server.arg("type");
+  Serial.println(key + " - " + type);  // 👈 网页触发串口的核心！
+  server.send(200);
+}
+
+void handleCmdStick() {
+  String stickId = server.arg("stickId");
+  String normX = server.arg("normX");
+  String normY = server.arg("normY");
+  Serial.println(stickId + " - X: " + normX + ", Y: " + normY); // 👈 网页触发串口的核心！
+  server.send(200);
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -74,6 +91,8 @@ void setup()
 
   // 启动网页服务
   server.onNotFound(handleWebRequest);
+  server.on("/cmd-button", handleCmdButton);
+  server.on("/cmd-stick", handleCmdStick);
   server.begin();
   Serial.println("Web 服务器已启动");
 }
